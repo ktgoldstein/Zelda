4d40571d1567d90cbb86a7d591f3d05998d4efc5
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2021-02-09 18:33:45 -0500

    Add skeleton for ILinkState interface: class headers for all concrete classes involving Link's 8 different normal and walking states, plus method headers for each concrete class.

--- LegendOfZeldaClone/ILinkState.cs
+++ LegendOfZeldaClone/ILinkState.cs
@@ -17,7 +17,340 @@ namespace LegendOfZeldaClone
 
     public class LinkNormalStandingDown : ILinkState
     {
+        public LinkNormalStandingDown()
+        {
 
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
+
+
+    }
+
+    public class LinkNormalStandingUp : ILinkState
+    {
+        public LinkNormalStandingUp()
+        {
+
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
+    }
+
+    public class LinkNormalStandingLeft : ILinkState
+    {
+
+        public LinkNormalStandingLeft()
+        {
+
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
+    }
+
+    public class LinkNormalStandingRight : ILinkState
+    {
+        public LinkNormalStandingRight()
+        {
+
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
+    }
+
+    public class LinkNormalWalkingDown : ILinkState
+    {
+        public LinkNormalWalkingDown()
+        {
+
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
+    }
+
+    public class LinkNormalWalkingUp : ILinkState
+    {
+        public LinkNormalWalkingUp()
+        {
+
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
+    }
+
+    public class LinkNormalWalkingLeft : ILinkState
+    {
+        public LinkNormalWalkingLeft()
+        {
+
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
+    }
+    public class LinkNormalWalkingRight : ILinkState
+    {
+        public LinkNormalWalkingRight()
+        {
+
+        }
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void Action()
+        {
+
+        }
+        public int Damage(int amount)
+        {
+
+
+            return 0;
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+
+        }
+        public void Update(Vector2 location)
+        {
+
+        }
     }
 
     // Simon edit after this comment
