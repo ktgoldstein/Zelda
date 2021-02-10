ba95a1fdb8da7d6bdd2d9004df9230b750338da1
alanw <alanw@DESKTOP-2UIPAIJ>
2021-02-10 17:48:16 -0500

    Added Aquamentus

--- LegendOfZeldaClone/IEnemies.cs
+++ LegendOfZeldaClone/IEnemies.cs
@@ -1,7 +1,12 @@
-﻿namespace LegendOfZeldaClone
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LegendOfZeldaClone
 {
     public interface IEnemies
     {
-
+        public void Update();
+        public void Draw(SpriteBatch spritebatch, Vector2 location);
+       
     }
 }
