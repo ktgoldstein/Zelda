7cc7906711c15055c366707ade910ba99d211eb8
alanw <alanw@DESKTOP-2UIPAIJ>
2021-02-11 01:19:02 -0500

    Added Stalfos, Aquamentus, Goriya

---
+++ LegendOfZeldaClone/Enemy/Boomerang.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZeldaClone.Enemy
+{
+    class Boomerang : IEnemy
+    {
+        private ISprite boomerangSprite;
+        private Vector2 location;
+        private Vector2 direction;
+        private float speed = 5;
+
+        public Boomerang(Vector2 location, Vector2 direction)
+        {
+            boomerangSprite = EnemySpriteFactory.Instance.createBoomerangSprite();
+            this.location = location;
+            this.direction = direction;
+            this.direction.Normalize();
+        }
+        public void Draw(SpriteBatch spritebatch)
+        {
+            boomerangSprite.Draw(spritebatch, location);
+        }
+
+        public void Update()
+        {
+            boomerangSprite.Update();
+            location += direction * speed;
+        }
+    }
+}
