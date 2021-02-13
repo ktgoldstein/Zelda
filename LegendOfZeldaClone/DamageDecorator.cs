5e13b5c4736e1039bfd871b6542e08ad2853c0ba
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2021-02-13 18:30:20 -0500

    Create DamageDecorator class.

---
+++ LegendOfZeldaClone/DamageDecorator.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace LegendOfZeldaClone
+{
+    public class DamageDecorator : IPlayer
+    {
+
+        public void MoveUp()
+        {
+
+        }
+        public void MoveDown()
+        {
+
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void MoveRight()
+        {
+
+        }
+        public void ActionA()
+        {
+
+        }
+        public void ActionB()
+        {
+
+        }
+        public void Damage(int amount)
+        {
+
+        }
+        public void Heal(int amount)
+        {
+
+        }
+        public void SetAItem(IUsableItem item)
+        {
+
+        }
+        public void SetBItem(IUsableItem item)
+        {
+
+        }
+        public void SetMaxHealth(int amount)
+        {
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+
+    }
+}
