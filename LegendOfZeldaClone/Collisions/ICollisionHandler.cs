3d581a44ebd92a067682faa4ec3127f8b4c1bc04
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2021-02-25 11:24:25 -0500

    Add Collisions folder; create ICollisionHandler interface; create base for one singleton implementation of ICollisionHandler (PlayerCollisionHandler)

---
+++ LegendOfZeldaClone/Collisions/ICollisionHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace LegendOfZeldaClone
+{
+    public interface ICollisionHandler
+    {
+        public void HandlePlayerCollision(IPlayer player);
+        public void HandlePlayerProjectileCollision(IPlayerProjectile playerProjectile);
+        public void HandleEnemyCollision(IEnemy enemy);
+        public void HandleEnemyProjectileCollision(IEnemy enemyProjectile);
+        public void HandleItemCollision(ISprite item); //tentative; IItem interface?
+        public void HandleBlockCollision(ISprite block); //tentative; IBlock interface?
+        public void HandleBoundaryCollision();
+    }
+}
+
