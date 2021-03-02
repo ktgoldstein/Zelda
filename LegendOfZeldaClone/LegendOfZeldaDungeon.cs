893fcb6b4114dcd956ee022609a39a0c11320999
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2021-03-02 15:31:45 -0500

    Add methods to detect collisions

--- LegendOfZeldaClone/LegendOfZeldaDungeon.cs
+++ LegendOfZeldaClone/LegendOfZeldaDungeon.cs
@@ -274,5 +274,47 @@ namespace LegendOfZeldaClone
 
             base.Draw(gameTime);
         }
+
+        private Direction DetectCollisionDirection(Vector2 location1, int width1, int height1, Vector2 location2, int width2, int height2)
+        {
+            float xOverlap = FindOverlap(location1.X, width1, location2.X, width2);
+            float yOverlap = FindOverlap(location1.Y, height1, location2.Y, height2);
+            Direction result = Direction.None;
+
+            if (xOverlap > 0 && yOverlap > 0)
+            {
+                if (xOverlap > yOverlap)
+                {
+                    result = (location1.X < location2.X) ? Direction.Left : Direction.Right;
+                }
+                else
+                {
+                    result = (location1.Y < location2.Y) ? Direction.Down : Direction.Up;
+                }
+            }
+
+            return result;
+        }
+
+        private float FindOverlap(float start1, int length1, float start2, int length2)
+        {
+            float difference = start1 - start2;
+            float overlap;
+
+            if (difference > 0)
+            {
+                overlap = length2 - difference;
+                if (overlap > length1)
+                    overlap = length1;
+            }
+            else
+            {
+                overlap = length1 + difference;
+                if (overlap > length2)
+                    overlap = length2;
+            }
+
+            return overlap;
+        }
     }
 }
