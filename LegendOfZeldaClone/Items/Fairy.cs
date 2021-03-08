4ffb60a8034e1d8dc2a77fcfff0863fc8dc725e7
Jaci Taylor <taylor.3136@buckeyemail.osu.edu>
2021-03-08 16:38:13 -0500

    Mostly randomized fairy movement. Also adjusted fairy collision with boundary (itemCollisionHandler)

--- LegendOfZeldaClone/Items/Fairy.cs
+++ LegendOfZeldaClone/Items/Fairy.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace LegendOfZeldaClone
 {
@@ -16,6 +17,9 @@ namespace LegendOfZeldaClone
         private readonly int height;
         private readonly int width;
         private readonly int speed;
+        private int timer;
+        private readonly Random rnd;
+        private int switchDirection;
 
         public Fairy(Vector2 location)
         {
@@ -26,28 +30,66 @@ namespace LegendOfZeldaClone
             FairyDirection = new Vector2(1, 1);
             speed = 2;
             Alive = true;
+            timer = 0;
+            rnd = new Random();
         }
 
         public void Update()
         {
             Location += speed * FairyDirection;
+            timer++;
 
             if (Location.Y > LoZHelpers.GameHeight)
             {
-                FairyDirection = new Vector2(1, -1);
+                FairyDirection = new Vector2(FairyDirection.X, -1);
             }
             else if (Location.Y < LoZHelpers.Scale(0))
             {
-                FairyDirection = new Vector2(1, 1);
+                FairyDirection = new Vector2(FairyDirection.X, 1);
             }
 
             if (Location.X > LoZHelpers.GameWidth)
             {
-                FairyDirection = new Vector2(-1, 1);
+                FairyDirection = new Vector2(-1, FairyDirection.Y);
             }
             else if (Location.X < LoZHelpers.Scale(0))
             {
-                FairyDirection = new Vector2(1, 1);
+                FairyDirection = new Vector2(1, FairyDirection.Y);
+            }
+
+            if (timer == 20)
+            {
+                switchDirection = rnd.Next(1, 8);
+                switch (switchDirection)
+                {
+                    case 1:
+                        FairyDirection = new Vector2(1, 1);
+                        break;
+                    case 2:
+                        FairyDirection = new Vector2(1, 0);
+                        break;
+                    case 3:
+                        FairyDirection = new Vector2(1, -1);
+                        break;
+                    case 4:
+                        FairyDirection = new Vector2(0, 1);
+                        break;
+                    case 5:
+                        FairyDirection = new Vector2(0, -1);
+                        break;
+                    case 6:
+                        FairyDirection = new Vector2(-1, 1);
+                        break;
+                    case 7:
+                        FairyDirection = new Vector2(-1, 0);
+                        break;
+                    case 8:
+                        FairyDirection = new Vector2(-1, -1);
+                        break;
+                    default:
+                        break;
+                }
+                timer = 0;
             }
 
             fairy.Update();
