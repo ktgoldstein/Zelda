0a7cac1b58ae5120f02588c765f3777b924224e3
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2021-03-07 22:24:54 -0500

    Functional operation of CollisionHandling.HandleCollisions

--- LegendOfZeldaClone/Collisions/CollisionHandling.cs
+++ LegendOfZeldaClone/Collisions/CollisionHandling.cs
@@ -12,8 +12,8 @@ namespace LegendOfZeldaClone.Collisions
             List<IEnemyProjectile> enemyProjectiles = game.EnemyProjectiles;
             List<IItem> items = game.Items;
             List<IObject> blocks = new List<IObject> { game.CurrentObject };
-            Boundary boundary;
-            
+
+            // Player collisions
             for (int playerIndex = 0; playerIndex < players.Count; playerIndex++)
             {
                 IPlayer player = players[playerIndex];
@@ -30,13 +30,13 @@ namespace LegendOfZeldaClone.Collisions
                         PlayerCollisionHandler.Instance.HandlePlayerCollision(player, collisionDirection);
                     }
                 }
+                PlayerCollisionHandler.Instance.CurrentPlayer = player;
                 foreach (IPlayerProjectile playerProjectile in playerProjectiles)
                 {
                     Direction collisionDirection = CollisionDetection.DetectCollisionDirection(player.HurtBoxLocation, player.Width, player.Height,
                         playerProjectile.Location, playerProjectile.Width, playerProjectile.Height);
                     if (collisionDirection != Direction.None)
                     {
-                        PlayerCollisionHandler.Instance.CurrentPlayer = player;
                         PlayerCollisionHandler.Instance.HandlePlayerProjectileCollision(playerProjectile, collisionDirection);
                         PlayerProjectileCollisionHandler.Instance.CurrentPlayerProjectile = playerProjectile;
                         PlayerProjectileCollisionHandler.Instance.HandlePlayerCollision(player, LoZHelpers.FlipDirection(collisionDirection));
@@ -48,7 +48,6 @@ namespace LegendOfZeldaClone.Collisions
                         enemy.Location, enemy.Width, enemy.Height);
                     if (collisionDirection != Direction.None)
                     {
-                        PlayerCollisionHandler.Instance.CurrentPlayer = player;
                         PlayerCollisionHandler.Instance.HandleEnemyCollision(enemy, collisionDirection);
                         EnemyCollisionHandler.Instance.CurrentEnemy = enemy;
                         EnemyCollisionHandler.Instance.HandlePlayerCollision(player, LoZHelpers.FlipDirection(collisionDirection));
@@ -60,10 +59,240 @@ namespace LegendOfZeldaClone.Collisions
                         enemyProjectile.Location, enemyProjectile.Width, enemyProjectile.Height);
                     if (collisionDirection != Direction.None)
                     {
-                        PlayerCollisionHandler.Instance.CurrentPlayer = player;
                         PlayerCollisionHandler.Instance.HandleEnemyProjectileCollision(enemyProjectile, collisionDirection);
                         EnemyProjectileCollisionHandler.Instance.CurrentEnemyProjectile = enemyProjectile;
                         EnemyProjectileCollisionHandler.Instance.HandlePlayerCollision(player, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IItem item in items)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(player.HurtBoxLocation, player.Width, player.Height,
+                        item.Location, item.Width, item.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        PlayerCollisionHandler.Instance.HandleItemCollision(item, collisionDirection);
+                        ItemCollisionHandler.Instance.CurrentItem = item;
+                        ItemCollisionHandler.Instance.HandlePlayerCollision(player, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IObject block in blocks)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(player.HurtBoxLocation, player.Width, player.Height,
+                        block.Location, block.Width, block.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        PlayerCollisionHandler.Instance.HandleObjectCollision(block, collisionDirection);
+                        ObjectCollisionHandler.Instance.CurrentObject = block;
+                        ObjectCollisionHandler.Instance.HandlePlayerCollision(player, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+            }
+
+            // PlayerProjectile collisions
+            for (int playerProjectileIndex = 0; playerProjectileIndex < playerProjectiles.Count; playerProjectileIndex++)
+            {
+                IPlayerProjectile playerProjectile = playerProjectiles[playerProjectileIndex];
+                for (int secondPlayerProjectileIndex = playerProjectileIndex + 1; secondPlayerProjectileIndex < playerProjectiles.Count; secondPlayerProjectileIndex++)
+                {
+                    IPlayerProjectile secondPlayerProjectile = playerProjectiles[secondPlayerProjectileIndex];
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(playerProjectile.Location, playerProjectile.Width, playerProjectile.Height,
+                        secondPlayerProjectile.Location, secondPlayerProjectile.Width, secondPlayerProjectile.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        PlayerProjectileCollisionHandler.Instance.CurrentPlayerProjectile = playerProjectile;
+                        PlayerProjectileCollisionHandler.Instance.HandlePlayerProjectileCollision(secondPlayerProjectile, collisionDirection);
+                        PlayerProjectileCollisionHandler.Instance.CurrentPlayerProjectile = secondPlayerProjectile;
+                        PlayerProjectileCollisionHandler.Instance.HandlePlayerProjectileCollision(playerProjectile, collisionDirection);
+                    }
+                }
+                PlayerProjectileCollisionHandler.Instance.CurrentPlayerProjectile = playerProjectile;
+                foreach (IEnemy enemy in enemies)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(playerProjectile.Location, playerProjectile.Width, playerProjectile.Height,
+                        enemy.Location, enemy.Width, enemy.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        PlayerProjectileCollisionHandler.Instance.HandleEnemyCollision(enemy, collisionDirection);
+                        EnemyCollisionHandler.Instance.CurrentEnemy = enemy;
+                        EnemyCollisionHandler.Instance.HandlePlayerProjectileCollision(playerProjectile, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IEnemyProjectile enemyProjectile in enemyProjectiles)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(playerProjectile.Location, playerProjectile.Width, playerProjectile.Height,
+                        enemyProjectile.Location, enemyProjectile.Width, enemyProjectile.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        PlayerProjectileCollisionHandler.Instance.HandleEnemyProjectileCollision(enemyProjectile, collisionDirection);
+                        EnemyProjectileCollisionHandler.Instance.CurrentEnemyProjectile = enemyProjectile;
+                        EnemyProjectileCollisionHandler.Instance.HandlePlayerProjectileCollision(playerProjectile, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IItem item in items)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(playerProjectile.Location, playerProjectile.Width, playerProjectile.Height,
+                        item.Location, item.Width, item.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        PlayerProjectileCollisionHandler.Instance.HandleItemCollision(item, collisionDirection);
+                        ItemCollisionHandler.Instance.CurrentItem = item;
+                        ItemCollisionHandler.Instance.HandlePlayerProjectileCollision(playerProjectile, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IObject block in blocks)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(playerProjectile.Location, playerProjectile.Width, playerProjectile.Height,
+                        block.Location, block.Width, block.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        PlayerProjectileCollisionHandler.Instance.HandleObjectCollision(block, collisionDirection);
+                        ObjectCollisionHandler.Instance.CurrentObject = block;
+                        ObjectCollisionHandler.Instance.HandlePlayerProjectileCollision(playerProjectile, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+            }
+
+            // Enemy collisions
+            for (int enemyIndex = 0; enemyIndex < enemies.Count; enemyIndex++)
+            {
+                IEnemy enemy = enemies[enemyIndex];
+                for (int secondEnemyIndex = enemyIndex + 1; secondEnemyIndex < enemies.Count; secondEnemyIndex++)
+                {
+                    IEnemy secondEnemy = enemies[secondEnemyIndex];
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(enemy.Location, enemy.Width, enemy.Height,
+                        secondEnemy.Location, secondEnemy.Width, secondEnemy.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        EnemyCollisionHandler.Instance.CurrentEnemy = enemy;
+                        EnemyCollisionHandler.Instance.HandleEnemyCollision(secondEnemy, collisionDirection);
+                        EnemyCollisionHandler.Instance.CurrentEnemy = secondEnemy;
+                        EnemyCollisionHandler.Instance.HandleEnemyCollision(enemy, collisionDirection);
+                    }
+                }
+                EnemyCollisionHandler.Instance.CurrentEnemy = enemy;
+                foreach (IEnemyProjectile enemyProjectile in enemyProjectiles)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(enemy.Location, enemy.Width, enemy.Height,
+                        enemyProjectile.Location, enemyProjectile.Width, enemyProjectile.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        EnemyCollisionHandler.Instance.HandleEnemyProjectileCollision(enemyProjectile, collisionDirection);
+                        EnemyProjectileCollisionHandler.Instance.CurrentEnemyProjectile = enemyProjectile;
+                        EnemyProjectileCollisionHandler.Instance.HandleEnemyCollision(enemy, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IItem item in items)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(enemy.Location, enemy.Width, enemy.Height,
+                        item.Location, item.Width, item.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        EnemyCollisionHandler.Instance.HandleItemCollision(item, collisionDirection);
+                        ItemCollisionHandler.Instance.CurrentItem = item;
+                        ItemCollisionHandler.Instance.HandleEnemyCollision(enemy, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IObject block in blocks)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(enemy.Location, enemy.Width, enemy.Height,
+                        block.Location, block.Width, block.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        EnemyCollisionHandler.Instance.HandleObjectCollision(block, collisionDirection);
+                        ObjectCollisionHandler.Instance.CurrentObject = block;
+                        ObjectCollisionHandler.Instance.HandleEnemyCollision(enemy, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+            }
+
+            // EnemyProjectile collisions
+            for (int enemyProjectileIndex = 0; enemyProjectileIndex < enemyProjectiles.Count; enemyProjectileIndex++)
+            {
+                IEnemyProjectile enemyProjectile = enemyProjectiles[enemyProjectileIndex];
+                for (int secondEnemyProjectileIndex = enemyProjectileIndex + 1; secondEnemyProjectileIndex < enemyProjectiles.Count; secondEnemyProjectileIndex++)
+                {
+                    IEnemyProjectile secondEnemyProjectile = enemyProjectiles[secondEnemyProjectileIndex];
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(enemyProjectile.Location, enemyProjectile.Width, enemyProjectile.Height,
+                        secondEnemyProjectile.Location, secondEnemyProjectile.Width, secondEnemyProjectile.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        EnemyProjectileCollisionHandler.Instance.CurrentEnemyProjectile = enemyProjectile;
+                        EnemyProjectileCollisionHandler.Instance.HandleEnemyProjectileCollision(secondEnemyProjectile, collisionDirection);
+                        EnemyProjectileCollisionHandler.Instance.CurrentEnemyProjectile = secondEnemyProjectile;
+                        EnemyProjectileCollisionHandler.Instance.HandleEnemyProjectileCollision(enemyProjectile, collisionDirection);
+                    }
+                }
+                EnemyProjectileCollisionHandler.Instance.CurrentEnemyProjectile = enemyProjectile;
+                foreach (IItem item in items)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(enemyProjectile.Location, enemyProjectile.Width, enemyProjectile.Height,
+                        item.Location, item.Width, item.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        EnemyProjectileCollisionHandler.Instance.HandleItemCollision(item, collisionDirection);
+                        ItemCollisionHandler.Instance.CurrentItem = item;
+                        ItemCollisionHandler.Instance.HandleEnemyProjectileCollision(enemyProjectile, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+                foreach (IObject block in blocks)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(enemyProjectile.Location, enemyProjectile.Width, enemyProjectile.Height,
+                        block.Location, block.Width, block.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        EnemyProjectileCollisionHandler.Instance.HandleObjectCollision(block, collisionDirection);
+                        ObjectCollisionHandler.Instance.CurrentObject = block;
+                        ObjectCollisionHandler.Instance.HandleEnemyProjectileCollision(enemyProjectile, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+            }
+
+            // Item collisions
+            for (int itemIndex = 0; itemIndex < items.Count; itemIndex++)
+            {
+                IItem item = items[itemIndex];
+                for (int secondItemIndex = itemIndex + 1; secondItemIndex < items.Count; secondItemIndex++)
+                {
+                    IItem secondItem = items[secondItemIndex];
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(item.Location, item.Width, item.Height,
+                        secondItem.Location, secondItem.Width, secondItem.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        ItemCollisionHandler.Instance.CurrentItem = item;
+                        ItemCollisionHandler.Instance.HandleItemCollision(secondItem, collisionDirection);
+                        ItemCollisionHandler.Instance.CurrentItem = secondItem;
+                        ItemCollisionHandler.Instance.HandleItemCollision(item, collisionDirection);
+                    }
+                }
+                ItemCollisionHandler.Instance.CurrentItem = item;
+                foreach (IObject block in blocks)
+                {
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(item.Location, item.Width, item.Height,
+                        block.Location, block.Width, block.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        ItemCollisionHandler.Instance.HandleObjectCollision(block, collisionDirection);
+                        ObjectCollisionHandler.Instance.CurrentObject = block;
+                        ObjectCollisionHandler.Instance.HandleItemCollision(item, LoZHelpers.FlipDirection(collisionDirection));
+                    }
+                }
+            }
+
+            // Block collisions
+            for (int blockIndex = 0; blockIndex < blocks.Count; blockIndex++)
+            {
+                IObject block = blocks[blockIndex];
+                for (int secondBlockIndex = blockIndex + 1; secondBlockIndex < blocks.Count; secondBlockIndex++)
+                {
+                    IObject secondBlock = blocks[secondBlockIndex];
+                    Direction collisionDirection = CollisionDetection.DetectCollisionDirection(block.Location, block.Width, block.Height,
+                        secondBlock.Location, secondBlock.Width, secondBlock.Height);
+                    if (collisionDirection != Direction.None)
+                    {
+                        ObjectCollisionHandler.Instance.CurrentObject = block;
+                        ObjectCollisionHandler.Instance.HandleObjectCollision(secondBlock, collisionDirection);
+                        ObjectCollisionHandler.Instance.CurrentObject = secondBlock;
+                        ObjectCollisionHandler.Instance.HandleObjectCollision(block, collisionDirection);
                     }
                 }
             }
