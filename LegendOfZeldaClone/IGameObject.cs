6e3adab6e7aaf806cd697eff064eb11da5f22043
simonksays123 <44527417+simonksays123@users.noreply.github.com>
2021-03-08 11:09:50 -0500

    Begin refactoring using IGameObject

---
+++ LegendOfZeldaClone/IGameObject.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LegendOfZeldaClone
+{
+    public interface IGameObject 
+    {
+        public Vector2 Location { get; set; }
+        public Vector2 HurtBoxLocation { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public void Draw(SpriteBatch spriteBatch);
+        public void Update();
+    }
+}
--- LegendOfZeldaClone/Players/IPlayer.cs
+++ LegendOfZeldaClone/Players/IPlayer.cs
@@ -3,15 +3,11 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace LegendOfZeldaClone
 {
-    public interface IPlayer
+    public interface IPlayer : IGameObject
     {
         public float Speed { get; set; }
         public int MaxHealth { get; set; }
         public int Health { get; set; }
-        public Vector2 Location { get; set; }
-        public Vector2 HurtBoxLocation { get; }
-        public int Width { get; }
-        public int Height { get; }
         public void MoveUp();
         public void MoveDown();
         public void MoveLeft();
@@ -22,7 +18,5 @@ namespace LegendOfZeldaClone
         public void Heal(int amount);
         //TODO: requires collision detection
         //public void PickUpItem(IUsableItem item);
-        public void Draw(SpriteBatch spriteBatch);
-        public void Update();
     }
 }
