0dbaa56350b12dfd88d5fca2b7db376b11efe0a7
alanw <alanw@DESKTOP-2UIPAIJ>
2021-03-07 19:25:37 -0500

    Refactoring Enemies

--- LegendOfZeldaClone/Enemies/IEnemy.cs
+++ LegendOfZeldaClone/Enemies/IEnemy.cs
@@ -8,8 +8,8 @@ namespace LegendOfZeldaClone
         public Vector2 Location { get; set; }
         public int Width { get; }
         public int Height { get; }
+        private int Health { get; set; }
         public void Update();
-        public void Draw(SpriteBatch spritebatch);       
+        public void Draw(SpriteBatch spritebatch);
     }
 }
-     
