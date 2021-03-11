cbaf2845e189c7ca9b288ceda1de6ae6102a2e40
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2021-03-11 15:22:53 -0500

    Add MovableRaisedBlock IObject implementation

---
+++ LegendOfZeldaClone/Objects/MovableRaisedBlock.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace LegendOfZeldaClone.Objects
+{
+    public class MovableRaisedBlock: IObject
+    {
+        public int Width { get { return LoZHelpers.Scale(width); } }
+        public int Height { get { return LoZHelpers.Scale(height); } }
+        public Vector2 Location { get; set; }
+        public Vector2 HurtBoxLocation
+        {
+            get { return Location; }
+            set { Location = value; }
+        }
+        public ObjectHeight BlockHeight { get; }
+        public bool IsMovable { get; }
+        public bool IsBombable { get; }
+
+        private ISprite raisedBlock;
+        private readonly int height;
+        private readonly int width;
+
+        public MovableRaisedBlock(Vector2 location)
+        {
+            raisedBlock = ObjectSpriteFactory.Instance.CreateRaisedBlock();
+            Location = location;
+            height = 16;
+            width = 16;
+            BlockHeight = ObjectHeight.CanFlyOver;
+            IsMovable = true;
+            IsBombable = false;
+        }
+        public void Update() { }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            raisedBlock.Draw(spriteBatch, Location);
+        }
+    }
+}
