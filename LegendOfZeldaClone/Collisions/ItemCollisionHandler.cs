7845b52e316f4a99d7dc7c9d5aa34918251c28dd
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2021-03-12 16:22:49 -0500

    Refactor ItemCollisionHandler to make use of new IObject BlockHeight property

--- LegendOfZeldaClone/Collisions/ItemCollisionHandler.cs
+++ LegendOfZeldaClone/Collisions/ItemCollisionHandler.cs
@@ -38,6 +38,40 @@ namespace LegendOfZeldaClone.Collisions
         public void HandleObjectCollision(IObject block, Direction direction)
         {
             //nothing will happen (fairies fly over blocks like keese do)
+            if (block.BlockHeight == ObjectHeight.Impassable)
+            {
+                if (CurrentItem is Fairy)
+                {
+                    Fairy fairy = CurrentItem as Fairy;
+                    switch (direction)
+                    {
+                        case Direction.Up:
+                            fairy.FairyDirection = new Vector2(fairy.FairyDirection.X, 1);
+                            break;
+                        case Direction.Down:
+                            fairy.FairyDirection = new Vector2(fairy.FairyDirection.X, -1);
+                            break;
+                        case Direction.Left:
+                            fairy.FairyDirection = new Vector2(1, fairy.FairyDirection.Y);
+                            break;
+                        case Direction.Right:
+                            fairy.FairyDirection = new Vector2(-1, fairy.FairyDirection.Y);
+                            break;
+                        case Direction.UpLeft:
+                            fairy.FairyDirection = new Vector2(1, 1);
+                            break;
+                        case Direction.UpRight:
+                            fairy.FairyDirection = new Vector2(-1, 1);
+                            break;
+                        case Direction.DownLeft:
+                            fairy.FairyDirection = new Vector2(1, -1);
+                            break;
+                        case Direction.DownRight:
+                            fairy.FairyDirection = new Vector2(-1, -1);
+                            break;
+                    }
+                }
+            }
         }
         public void HandleBoundaryCollision(Boundary boundary, Direction direction)
         {
