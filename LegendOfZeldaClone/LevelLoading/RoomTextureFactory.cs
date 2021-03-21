0e29e20b854fe5949b8043c229924c8c81f02c3d
Kristin <kriss@LAPTOP-POI5VGLR>
2021-03-21 19:52:50 -0400

    Added back accidentially deleted lines from LevelLoading

--- LegendOfZeldaClone/LevelLoading/RoomTextureFactory.cs
+++ LegendOfZeldaClone/LevelLoading/RoomTextureFactory.cs
@@ -25,5 +25,8 @@ namespace LegendOfZeldaClone.LevelLoading
             tiles = content.Load<Texture2D>("Background");
             background = content.Load<Texture2D>("Background");
         }
+
+        public ISprite CreateTiles() => new BackgroundSprite(tiles, 2, 192, 192, 112);
+        public ISprite CreateWalls() => new BackgroundSprite(background, 522, 11, 256, 176);
     }
 }
