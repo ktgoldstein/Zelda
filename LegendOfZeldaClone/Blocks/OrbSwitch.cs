7d9a7eb7d4572d90501f8246f51c0be74aad5d6d
Jaci Taylor <taylor.3136@buckeyemail.osu.edu>
2021-04-17 15:40:12 -0400

    adding in orb class

---
+++ LegendOfZeldaClone/Blocks/OrbSwitch.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegendOfZeldaClone
+{
+    public class OrbSwitch : BlockKernel
+    {
+        public bool WasHit { get; set; } = false;
+
+        public OrbSwitch(Vector2 location, ISprite sprite, int height, int width, ObjectHeight objectHeight)
+            :base (location, sprite, height, width, objectHeight, false, false)
+        { }
+        public void Reset() => WasHit = false;
+    }
+}
