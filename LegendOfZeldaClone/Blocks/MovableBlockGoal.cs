f6592c9bb8c1c4df24356031126dec8d5d1b814b
Yonace Ghebrehiwet <ghebrehiwet.2@buckeyemail.osu.edu>
2021-04-19 10:56:23 -0400

    MazeRoom 4,6 door changes and removal of movableblockgoal dark block draw

--- LegendOfZeldaClone/Blocks/MovableBlockGoal.cs
+++ LegendOfZeldaClone/Blocks/MovableBlockGoal.cs
@@ -5,13 +5,11 @@ namespace LegendOfZeldaClone
 {
     public class MovableBlockGoal : BlockKernel
     {
-        private ISprite test;
         public MovableBlockGoal(Vector2 location, int height, int width, ObjectHeight objectHeight)
             : base(location, null, height, width, objectHeight, false, false) {
-            test = BlockSpriteFactory.Instance.CreateDarkBlock();
         }
 
-        public override void Draw(SpriteBatch spriteBatch) { test.Draw(spriteBatch, Location); }
-        public override void DrawAt(SpriteBatch spriteBatch, Vector2 location) { test.Draw(spriteBatch, location); }
+        public override void Draw(SpriteBatch spriteBatch) {}
+        public override void DrawAt(SpriteBatch spriteBatch, Vector2 location) {}
     }
 }
