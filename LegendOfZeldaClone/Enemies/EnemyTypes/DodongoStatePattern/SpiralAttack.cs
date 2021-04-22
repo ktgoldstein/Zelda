a9b3ba795b307203f44c99f01f04132388706ae5
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2021-04-22 12:10:24 -0400

    Remove unused code from SpiralAttack.cs

--- LegendOfZeldaClone/Enemies/EnemyTypes/DodongoStatePattern/SpiralAttack.cs
+++ LegendOfZeldaClone/Enemies/EnemyTypes/DodongoStatePattern/SpiralAttack.cs
@@ -12,16 +12,11 @@ namespace LegendOfZeldaClone.Enemies.EnemyTypes.DodongoStatePattern
         private readonly Dodongo dodongo;
         private Vector2 Location;
         private int timer = 120;
-        private readonly Direction[] attackDirections = { Direction.Up, Direction.Right, Direction.Down, Direction.Left };
-        private Direction currentAttackDirection;
-        private int currentAttackIndex;
         private int angle = 360;
         public SpiralAttack(Dodongo dodongo, GameStateMachine game)
         {
             this.dodongo = dodongo;
             this.game = game;
-            currentAttackIndex = 0;
-            currentAttackDirection = attackDirections[currentAttackIndex];
             Location = dodongo.Location;
         }
         public void Update()
@@ -29,7 +24,6 @@ namespace LegendOfZeldaClone.Enemies.EnemyTypes.DodongoStatePattern
             angle -= 3;
             if (timer % 3 == 0)
                 ShootFireball();
-            UpdateCurrentAttackDirection();
             if (timer == 0)
                 dodongo.State = new PhaseOneIdle(dodongo, game);
             timer--;
@@ -38,17 +32,6 @@ namespace LegendOfZeldaClone.Enemies.EnemyTypes.DodongoStatePattern
         {
             Vector2 direction = new Vector2((float)Math.Cos(angle * Math.PI / 180), (float)Math.Sin(angle * Math.PI / 180));
             game.EnemyProjectilesQueue.Add(new Fireball(Location, direction));
-        }
-        private void UpdateCurrentAttackDirection()
-        {
-            if (timer % 20 == 0)
-            {
-                if (currentAttackIndex < 3)
-                    currentAttackIndex++;
-                else
-                    currentAttackIndex = 0;
-                currentAttackDirection = attackDirections[currentAttackIndex];
-            }
         }
     }
 }
